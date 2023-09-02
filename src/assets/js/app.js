3529b3330d4b5d3086cdfbfa31c67a26f019ff82
Brandon Lowe <BLoweCLE@gmail.com>
2023-09-02 07:59:22 -0500

    hosting on netlify

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,5 @@
+import gsap from 'gsap';
+
 const bar = document.querySelector('.loading__bar--inner');
 const counter_number = document.querySelector('.loading__counter--number');
 let count = 0;
@@ -9,6 +11,44 @@ let barInterval = setInterval(() => {
     count++;
     if (count > 100) {
         clearInterval(barInterval);
+        gsap.to('.loading',{
+            delay: 4,
+            zIndex: 1,
+        })
+        gsap.to('.loading__bar',{
+            duration: 5,
+            rotate: "90deg",
+            left: "1000%",
+            top: "-600%",
+        });
+        gsap.to('.loading__text,.loading__counter',{
+            duration: .5,
+            opacity: 0
+        });
+        gsap.to('.loading__box',{
+            duration: .5,
+            borderRadius: "50%",
+            height: "500px",
+            width: "500px"
+        })
+        gsap.to('.loading__svg', {
+            duration: 6,
+            opacity: 1,
+        })
+        gsap.to('.loading__svg', {
+            duration: 1,
+            rotate: "360deg",
+        })
+        gsap.to('.loading__box',{
+            delay: 1,
+            duration: .3,
+            borderWidth: 0,
+        })
+        gsap.to('.loading', {
+            delay: 2,
+            duration: 2,
+            background: 'transparent',
+        })
     }
 
 }, 30);
--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -1,2 +1,5 @@
 import '../styles/base.scss';
 import '../styles/loader.scss';
+import '../styles/landing.scss';
+import '../styles/coolLink.scss';
+import '../styles/skills.scss';
