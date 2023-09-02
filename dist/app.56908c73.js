d2ab0788618d85b26e7f56c7980947dd741ebd35
Brandon Lowe <BLoweCLE@gmail.com>
2023-09-02 08:05:34 -0500

    test

--- dist/app.56908c73.js
+++ dist/app.56908c73.js
@@ -5617,7 +5617,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60413" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61437" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
